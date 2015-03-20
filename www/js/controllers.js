1874ac203dd5b68f023bccc18518bdfecaefba8e
Michal Lipski <kiwaczki@gmail.com>
2015-03-20 09:47:24 +0100

    getting gps position

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,18 +1,31 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+    .controller('DashCtrl', function ($scope) {
+    })
 
-.controller('HospitalsCtrl', function($scope, Hospitals) {
-  $scope.hospitals = Hospitals.all();
+    .controller('HospitalsCtrl', function ($scope, Hospitals) {
+        $scope.hospitals = Hospitals.all();
 
-})
+    })
 
-.controller('HospitalDetailCtrl', function($scope, $stateParams, Hospitals) {
-  $scope.hospital = Hospitals.get($stateParams.hospitalId);
-})
+    .controller('HospitalDetailCtrl', function ($scope, $stateParams, Hospitals) {
+        $scope.hospital = Hospitals.get($stateParams.hospitalId);
+    })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+    .controller('AccountCtrl', function ($scope) {
+
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+
+        function onError(error) {
+            alert('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n');
+        }
+
+        function onSuccess(position) {
+            $scope.settings.position = position;
+            $scope.$apply();
+        }
+
+        $scope.settings = {
+            enableFriends: true
+        };
+    });
